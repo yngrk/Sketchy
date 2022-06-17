49e7a9f0c791bac19cc98f69b88841d8111573f9
Young Rok Kim <young@MacBook-Air-von-Young.local>
2022-06-17 21:00:17 +0200

    added some comments for later

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function createGrid(amount) {
+    // creates a grid layout of (amount) of divs
     const container = document.createElement('div');
     container.classList.add('grid-container');
 
@@ -13,8 +14,10 @@ function createGrid(amount) {
     document.body.appendChild(container);
 }
 
+// change this part, so user can decide.
 createGrid(10000);
 
+// add eventListener, so that grid changes color on Mouseover
 const boxes = document.querySelectorAll('.grid-item');
 boxes.forEach(box => box.addEventListener('mouseenter', function(event) {
     event.target.style['background-color'] = 'white';
