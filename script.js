fe717876543050263149ee9f7c5a50039009bf9e
Young Rok Kim <young@MacBook-Air-von-Young.local>
2022-06-18 12:49:25 +0200

    Added Resolution Change Functionality

--- script.js
+++ script.js
@@ -15,11 +15,33 @@ function createGrid(amount) {
 }
 
 createGrid(128);
+addHoverEffect();
 
 // add eventListener, so that grid changes color on Mouseover
-const boxes = document.querySelectorAll('.grid-item');
-boxes.forEach(box => box.addEventListener('mouseenter', function(event) {
-    event.target.style['background-color'] = 'white';
-}));
+// make it a function so it can be called when resolution is updated
+function addHoverEffect() {
+    const boxes = document.querySelectorAll('.grid-item');
+    boxes.forEach(box => box.addEventListener('mouseenter', function(event) {
+        event.target.style['background-color'] = 'white';
+    }));
+}
+
+// add function to remove existing grid
+function clear() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    const gridContainer = document.querySelector('.grid-container');
+    gridItems.forEach(grid => grid.remove());
+    gridContainer.remove();
+}
 
 // add eventListener to Resolution-Button to change the resolution of the canvas
+const resButton = document.querySelector('.res');
+resButton.addEventListener('click', function(event) {
+    clear();
+    let gridSize = prompt('Enter Resolution (max: 10000)');
+    if (gridSize > 10000) {
+        gridSize = 10000;
+    }
+    createGrid(parseInt(gridSize));
+    addHoverEffect();
+});
